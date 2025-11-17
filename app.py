f71803e58cbbbdc84f75d764d64946c563ad0108
Satvik Agarwal <73279834+SatvikAgarwa@users.noreply.github.com>
2025-11-17 10:52:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import os
 
 app = Flask(__name__)
 
-cache = Cache(app, config={"CACHE_TYPE": "simple"})
-
 @app.route("/")
 def home():
     top_gainers = get_top_gainers()
@@ -48,5 +46,6 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
     app.run(debug=True)
+
 
 
